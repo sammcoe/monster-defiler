c865cf92298102e89b39dba2acc504818684a998
Sam Coe <samuel.m.coe@gmail.com>
2016-11-06 15:03:43 -0600

    Fix

--- public/js/RemotePlayer.js
+++ public/js/RemotePlayer.js
@@ -10,7 +10,7 @@ var RemotePlayer = function (index, game, player, startX, startY, startAngle) {
   this.player = player
   this.alive = true
 
-  this.player = game.add.sprite(x, y, 'enemy')
+  this.player = MonsterDefiler.game.add.sprite(x, y, 'enemy')
 
   this.player.animations.add('move', [0, 1, 2, 3, 4, 5, 6, 7], 20, true)
   this.player.animations.add('stop', [3], 20, true)
@@ -18,7 +18,7 @@ var RemotePlayer = function (index, game, player, startX, startY, startAngle) {
   this.player.anchor.setTo(0.5, 0.5)
 
   this.player.name = index.toString()
-  game.physics.enable(this.player, Phaser.Physics.ARCADE)
+  MonsterDefiler.game.physics.enable(this.player, Phaser.Physics.ARCADE)
   this.player.body.immovable = true
   this.player.body.collideWorldBounds = true
 
@@ -30,7 +30,7 @@ var RemotePlayer = function (index, game, player, startX, startY, startAngle) {
 RemotePlayer.prototype.update = function () {
   if (this.player.x !== this.lastPosition.x || this.player.y !== this.lastPosition.y || this.player.angle != this.lastPosition.angle) {
     this.player.play('move')
-    this.player.rotation = Math.PI + game.physics.arcade.angleToXY(this.player, this.lastPosition.x, this.lastPosition.y)
+    this.player.rotation = Math.PI + MonsterDefiler.game.physics.arcade.angleToXY(this.player, this.lastPosition.x, this.lastPosition.y)
   } else {
     this.player.play('stop')
   }
